bedbbb224fe775475f89ebf87a04caf016bfa651
Alexander <alexandercharlesworth@gmail.com>
2024-09-26 17:51:03 +0100

    CreateUserForm extends User.

--- src/main/java/alexcharlesworth/rest/pojos/User.java
+++ src/main/java/alexcharlesworth/rest/pojos/User.java
@@ -1,17 +1,45 @@
 package alexcharlesworth.rest.pojos;
 
-/*Stores User details which are returned to the frontend*/
 public class User {
 
-    private final String email;
-    private final String first_name;
-    private final String surname;
-    private final String password;
+    private String email;
+    private String first_name;
+    private String surname;
+
+    private String password;
 
     public User(String email, String first_name, String surname, String password) {
         this.email = email;
         this.first_name = first_name;
         this.surname = surname;
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public void setPassword(String password) {
         this.password = password;
     }
 
@@ -21,6 +49,6 @@ public class User {
                 "email='" + email + '\'' +
                 ", first_name='" + first_name + '\'' +
                 ", surname='" + surname + '\'' +
-                '}';
+                 '}';
     }
 }
